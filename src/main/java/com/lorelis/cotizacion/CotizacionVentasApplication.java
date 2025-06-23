5e0d398419f219df47e045bce2f446b8bf675366
Lorena Sanchez <sanlo26@outlook.es>
2025-06-23 10:59:39 -0500

    correccion zona horaria

--- src/main/java/com/lorelis/cotizacion/CotizacionVentasApplication.java
+++ src/main/java/com/lorelis/cotizacion/CotizacionVentasApplication.java
@@ -1,10 +1,19 @@
 package com.lorelis.cotizacion;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CotizacionVentasApplication {
+
+	@PostConstruct
+	public void init() {
+		// Establecer zona horaria por defecto
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CotizacionVentasApplication.class, args);
